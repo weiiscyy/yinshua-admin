4b0973c5061bcd866b4cbe7c36df2f4b28487420
Weii <weii@example.com>
2026-04-27 20:10:18 +0800

    fix: ZM首检记录融入生产规格grid布局(跨满一行)

--- src/pages/OrderEntryPage.jsx
+++ src/pages/OrderEntryPage.jsx
@@ -792,14 +792,8 @@ export default function OrderEntryPage() {
                         <Form.Item label="花长" name="huachang" style={{ marginBottom: 4 }}><Input placeholder="花长" /></Form.Item>
                         <Form.Item label="成品尺寸" name="chenpingcc" style={{ marginBottom: 4 }}><Input placeholder="成品尺寸" /></Form.Item>
                         <Form.Item label="加工费" name="jiagongfei" style={{ marginBottom: 4 }}><Input placeholder="加工费" type="number" step="0.0001" /></Form.Item>
+                        <Form.Item label="首检记录" name="soujianjl" style={{ marginBottom: 4, gridColumn: '1 / -1' }}><Input placeholder="首检记录" /></Form.Item>
                       </div>
-                    </div>
-
-                    {/* 首检记录 */}
-                    <div style={{ marginBottom: 16 }}>
-                      <Form.Item label="首检记录" name="soujianjl" style={{ marginBottom: 4 }}>
-                        <Input placeholder="首检记录" />
-                      </Form.Item>
                     </div>
 
                     {/* 色卡明细表 */}
